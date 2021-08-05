3859362715a0bdab1d2c187b9560466731f5dfd1
Darren Goh <darrengohyaowen1508@gmail.com>
2021-08-06 00:46:40 +0800

    Song searched by edittext

--- app/src/main/java/com/example/musicstream/SearchPageActivity.java
+++ app/src/main/java/com/example/musicstream/SearchPageActivity.java
@@ -6,21 +6,46 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchPageActivity extends AppCompatActivity {
     private RecyclerView searchrecycleview;
-    private RecyclerView.Adapter searchAdapter;
+    private SearchRecycleView searchAdapter;
     private RecyclerView.LayoutManager searchlayoutManager;
     SongCollection songCollection = new SongCollection();
     List<Song> songList = songCollection.getSongtrack();
+    int searchartistsong;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
+        Bundle searchsong = this.getIntent().getExtras();
+        searchartistsong = searchsong.getInt("index");
+        EditText editText = findViewById(R.id.searchtext);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                filter(s.toString());
+            }
+        });
         searchrecycleview = findViewById(R.id.searchrecycleview);
         searchrecycleview.setHasFixedSize(true);
         searchlayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
@@ -28,4 +53,20 @@ public class SearchPageActivity extends AppCompatActivity {
         searchAdapter = new SearchRecycleView(songList,this);
         searchrecycleview.setAdapter(searchAdapter);
     }
+
+    private void filter(String text)
+    {
+        ArrayList<Song> searchedList = new ArrayList<>();
+        for(Song songitem: songList)
+        {
+            if(songitem.getTitle().toLowerCase().contains(text.toLowerCase()))
+            {
+                searchedList.add(songitem);
+            }
+        }
+        searchAdapter.searchedList(searchedList);
+
+    }
+
+
 }
--- app/src/main/java/com/example/musicstream/SearchRecycleView.java
+++ app/src/main/java/com/example/musicstream/SearchRecycleView.java
@@ -1,19 +1,23 @@
 package com.example.musicstream;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchRecycleView extends RecyclerView.Adapter<SearchRecycleView.MyViewHolder> {
+    SongCollection songCollection = new SongCollection();
     List<Song> songList;
     Context context;
     public SearchRecycleView(List<Song> songList, Context context)
@@ -30,15 +34,33 @@ public class SearchRecycleView extends RecyclerView.Adapter<SearchRecycleView.My
     }
     @Override
     public void onBindViewHolder(@NonNull SearchRecycleView.MyViewHolder searchholder, int position) {
+        String id = songList.get(position).getId();
         searchholder.searchArtistimage.setImageResource(songList.get(position).getDrawable());
         searchholder.searchSongtitle.setText(String.valueOf(songList.get(position).getTitle()));
         searchholder.searchArtistname.setText(String.valueOf(songList.get(position).getArtiste()));
+        searchholder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Toast.makeText(context, songList.get(position).getTitle(), Toast.LENGTH_SHORT).show();
+                int index = songCollection.searchSongById(id);
+                Intent aftersearchsongpage = new Intent(context, PlaySongActivity.class);
+                aftersearchsongpage.putExtra("index", index);
+                context.startActivity(aftersearchsongpage);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return songList.size();
     }
+
+    public void searchedList(ArrayList<Song> searchedList)
+    {
+        songList = searchedList;
+        notifyDataSetChanged();
+    }
+
     public class MyViewHolder extends RecyclerView.ViewHolder
     {
         ImageView searchArtistimage;
@@ -54,6 +76,5 @@ public class SearchRecycleView extends RecyclerView.Adapter<SearchRecycleView.My
             searchSongtitle = itemView.findViewById(R.id.searchrvsongname);
             parentLayout = itemView.findViewById(R.id.searchrvconstraintlayout);
         }
-
     }
 }
